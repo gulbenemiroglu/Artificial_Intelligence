aa2587209c3d82787044d041172a00813fd1c73e
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2024-12-11 21:13:22 +0300

    updated

--- BasicPythonLecture_ByMe/Python101/p09_dictionaries.py
+++ BasicPythonLecture_ByMe/Python101/p09_dictionaries.py
@@ -120,6 +120,133 @@ print(x) #after the change
 
 
 
+# =============================================================================
+# Check if Key Exists - "in" Keyword
+# =============================================================================
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+
+# =============================================================================
+# Change Values
+# =============================================================================
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+
+# =============================================================================
+# Update Dictionary
+# =============================================================================
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+
+# =============================================================================
+# Adding Items
+# =============================================================================
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+# The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+
+
+# =============================================================================
+# Removing Items
+# =============================================================================
+
+# The pop() method
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+
+# The del keyword removes the item with the specified key name:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+# The del keyword can also delete the dictionary completely:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists
+
+# The clear() method empties the dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
