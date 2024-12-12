f552d480f33bd6696a0d426a82d96fb424585414
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2024-12-12 17:53:59 +0300

    p10_functions

--- BasicPythonLecture_ByMe/Python101/p10_functions.py
+++ BasicPythonLecture_ByMe/Python101/p10_functions.py
@@ -6,3 +6,21 @@ def my_function():
   print("Hello from a function")
   
 my_function()
+
+# =============================================================================
+# Arguments
+# =============================================================================
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+# Arbitrary Arguments, *args
+# If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
