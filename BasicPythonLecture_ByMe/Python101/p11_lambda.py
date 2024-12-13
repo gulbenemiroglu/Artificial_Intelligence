2301a4e22eb1ac74fb4444faaf44753785a9f205
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2024-12-13 14:55:54 +0300

    lambda file added

--- BasicPythonLecture_ByMe/Python101/p11_lambda.py
+++ BasicPythonLecture_ByMe/Python101/p11_lambda.py
@@ -7,3 +7,33 @@
 
 x = lambda a : a + 10
 print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+
+
+# =============================================================================
+# Why Use Lambda Functions?
+# =============================================================================
+
+# The power of lambda is better shown when you use them as an anonymous function inside another function.
+# Say you have a function definition that takes one argument, and that argument will be multiplied with an unknown number.
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
