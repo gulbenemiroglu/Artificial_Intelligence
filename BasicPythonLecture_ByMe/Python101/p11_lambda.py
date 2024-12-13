508381c10e42f1c73a2202e1a03264bb1bc73f56
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2024-12-13 14:49:16 +0300

    p11_lambda

---
+++ BasicPythonLecture_ByMe/Python101/p11_lambda.py
@@ -0,0 +1,9 @@
+# =============================================================================
+#       PYTHON LAMBDA
+# =============================================================================
+
+# A lambda function can take any number of arguments, but can only have one expression.
+# lambda arguments : expression
+
+x = lambda a : a + 10
+print(x(5))
