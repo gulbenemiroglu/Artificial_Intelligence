90f9d7ebf92e9f6f552e1db042681d92c3d190bc
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2024-12-16 07:09:45 +0300

    p14_inharitance

--- BasicPythonLecture_ByMe/Python101/p14_inheritance.py
+++ BasicPythonLecture_ByMe/Python101/p14_inheritance.py
@@ -7,3 +7,42 @@
 # Parent class is the class being inherited from, also called base class.
 
 # Child class is the class that inherits from another class, also called derived class.
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+
+# Create a Child Class
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+# Use the super() Function
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+# Add Properties
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
